28095ac97d3429177cf2c3f1a79a5497c12933a5
Lucas Lofaro <lucasmlofaro@gmail.com>
2019-11-24 01:01:41 -0800

    app: Use NBA object for API access

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
+import os
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-import nba_api
+from nba_api import NBA
 
 app = Flask(__name__)
+
+# TODO: Change DEBUG default to false
+DEBUG = os.environ.get('DEBUG', True)
+API_KEY = os.environ.get('API_KEY')
+
+NBA_API = NBA(API_KEY)
 
 
 @app.route('/games')
 def games():
-    today = datetime.today().date()
-    data = nba_api.games_by_date(str(today))
+    date = request.args.get('date')
+
+    if date:
+        # TODO: Handle parsing error
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+    else:
+        # TODO: Deal with timezones
+        date = datetime.utcnow().date()
+
+    data = NBA_API.games_by_date(date)
     return render_template('games.html', games=data)
 
 
 if __name__ == '__main__':
-    # TODO: Use a DEBUG environment variable
-    app.run(debug=True)
+    app.run(debug=DEBUG)
